8b626742fb77c7fe52837396d71b4e3b3c9fac21
AtharvaPatil86 <atharvapatil619@gmail.com>
2025-10-06 02:39:04 +0530

    Added Day and Date

--- Clock/index.js
+++ Clock/index.js
@@ -1,13 +1,21 @@
 setInterval(() => {
-    d = new Date();
-    htime = d.getHours();
-    mtime = d.getMinutes();
-    stime = d.getSeconds();
-    hrotation = 30*htime + mtime/2;
-    mrotation = 6*mtime;
-    srotation = 6*stime;
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+    let hrotation = 30*htime + mtime/2;
+    let mrotation = 6*mtime;
+    let srotation = 6*stime;
 
-    hour.style.transform = `rotate(${hrotation}deg)`;
-    minute.style.transform = `rotate(${mrotation}deg)`;
-    second.style.transform = `rotate(${srotation}deg)`;
+    document.getElementById("hour").style.transform = `rotate(${hrotation}deg)`;
+    document.getElementById("minute").style.transform = `rotate(${mrotation}deg)`;
+    document.getElementById("second").style.transform = `rotate(${srotation}deg)`;
+
+    // Day and date
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const day = days[d.getDay()];
+    const date = d.getDate(); // You can also add month/year if needed
+
+    document.getElementById("day").innerText = day;
+    document.getElementById("date").innerText = date;
 }, 1000);
